c73addfa8ae71be42d7278a34ab90772bd6f3614
Aldair Enriquez Duran <aldair_fime@hotmail.com>
2021-07-28 17:03:45 -0500

    Se termino home y pagina de comunicaciones, es necesario optimizacion para moviles

--- Public/public/private/comunicaciones.js
+++ Public/public/private/comunicaciones.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    
     let topnavIconBtn = document.getElementById("icon-btn");
 
     topnavIconBtn.addEventListener("click", function() {
@@ -16,12 +15,4 @@ window.onload = function() {
             x.className = "topnav";
         }
     }
-
-    let abrirComunicaciones = document.getElementById("abrirComunicaciones");
-
-    abrirComunicaciones.addEventListener("click", function() {
-
-        window.open("http://127.0.0.1:5501/public/comunicaciones.html", "_self");
-
-    });
 }
