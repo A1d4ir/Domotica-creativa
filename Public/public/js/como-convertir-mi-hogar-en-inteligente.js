620df8921ceedd81437eb522606299a9830d8415
Aldair Enriquez Duran <aldair_fime@hotmail.com>
2021-09-10 15:52:08 -0500

    Se completaron los articulos: 'internet de las cosas' y 'que es domotica e inmotica'

--- Public/public/js/como-convertir-mi-hogar-en-inteligente.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    
-    document.querySelector('.menu-btn').addEventListener('click', () => {
-        document.querySelector('.nav-menu').classList.toggle('show');
-    });
-
-}
--- Public/public/js/internet-de-las-cosas.js
+++ Public/public/js/internet-de-las-cosas.js
@@ -1,10 +1,8 @@
-
+    
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
 
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.cards-banner-one', {delay: 500});
 ScrollReveal().reveal('.cards-banner-two', {delay: 500});
 ScrollReveal().reveal('.card-productos', {delay: 500});
-ScrollReveal().reveal('.imagen', {delay: 500});
